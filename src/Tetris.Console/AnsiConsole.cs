27649b52cfc14bc1fd39879aa1df0e7ad601e4dc
Ales Kobr <kobr.ales@hotmail.cz>
2021-04-16 01:51:54 +0100

    added initial console with ANSI code support

---
+++ src/Tetris.Console/AnsiConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SystemConsole = System.Console;
+
+namespace Tetris.Console
+{
+    public class AnsiConsole
+    {
+        private const string EscapeCharacter = "\u001b";
+
+        public void ClearEntireScreen()
+        {
+            SystemConsole.Write($"{EscapeCharacter}[J2");
+        }
+
+        public void ClearScreenUp()
+        {
+            SystemConsole.Write($"{EscapeCharacter}[J1");
+        }
+
+        public void MoveCursorToPreviousLine()
+        {
+            SystemConsole.Write($"{EscapeCharacter}[F1");
+        }
+
+        public void MoveCursorToNextLine()
+        {
+            SystemConsole.Write($"{EscapeCharacter}[E1");
+        }
+    }
+}
